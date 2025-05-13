8a31bd4ddea55e5a7f09fc136a062f585af9c403
Jordan Roberts <jrobertswebdev@gmail.com>
2025-05-13 13:38:27 -0500

    download multiple media if available and selected, then zip

--- src/constants.ts
+++ src/constants.ts
@@ -1,21 +1,21 @@
 export const CONFIG_LIST = [
-  'setting_show_open_in_new_tab_icon',
-  'setting_enable_threads',
-  'setting_enable_video_controls',
-  'setting_format_replace_jpeg_with_jpg',
-  'setting_format_use_hash_id',
-  'setting_format_use_indexing',
-  'setting_format_use_datetime',
-  'setting_enable_download_multiple_media',
-];
+ 'setting_show_open_in_new_tab_icon',
+ 'setting_enable_threads',
+ 'setting_enable_video_controls',
+ 'setting_format_replace_jpeg_with_jpg',
+ 'setting_format_use_hash_id',
+ 'setting_format_use_indexing',
+ 'setting_format_use_datetime',
+ 'setting_enable_download_multiple_media',
+]
 
-export const DEFAULT_FILENAME_FORMAT = `{username}-{id}-{datetime}`;
-export const DEFAULT_DATETIME_FORMAT = 'YYYY-MM-DD_hh:mm:ss';
+export const DEFAULT_FILENAME_FORMAT = `{username}-{id}-{datetime}`
+export const DEFAULT_DATETIME_FORMAT = 'YYYY-MM-DD_hh:mm:ss'
 
 // local extension id
-// export const EXTENSION_ID = 'cefefedmkcjnojhnekdhkepfckbcmbgc';
+export const EXTENSION_ID = 'cefefedmkcjnojhnekdhkepfckbcmbgc'
 
 // store extension id
-export const EXTENSION_ID = 'immfpkjakbknecheoampmdjpnigaoend';
+// export const EXTENSION_ID = 'immfpkjakbknecheoampmdjpnigaoend';
 
-export const CLASS_CUSTOM_BUTTON = 'custom-btn';
+export const CLASS_CUSTOM_BUTTON = 'custom-btn'
--- src/content/reels.ts
+++ src/content/reels.ts
@@ -1,92 +1,86 @@
-import dayjs from 'dayjs';
-import {
-  DownloadParams,
-  downloadResource,
-  fetchHtml,
-  getMediaName,
-  getUrlFromInfoApi,
-  openInNewTab,
-} from './utils';
-import type { Reels } from '../types/reels';
+import dayjs from 'dayjs'
+import { DownloadParams, downloadResource, fetchHtml, getMediaName, getUrlFromInfoApi, openInNewTab } from './utils'
+import type { Reels } from '../types/reels'
 
-function findReels(
-  obj: Record<string, any>
-): Reels.XdtApiV1ClipsHomeConnectionV2 | undefined {
-  for (const key in obj) {
-    if (key === 'xdt_api__v1__clips__home__connection_v2') {
-      return obj[key];
-    } else if (typeof obj[key] === 'object' && obj[key] !== null) {
-      const result = findReels(obj[key]);
-      if (result) {
-        return result;
-      }
-    }
+function findReels(obj: Record<string, any>): Reels.XdtApiV1ClipsHomeConnectionV2 | undefined {
+ for (const key in obj) {
+  if (key === 'xdt_api__v1__clips__home__connection_v2') {
+   return obj[key]
+  } else if (typeof obj[key] === 'object' && obj[key] !== null) {
+   const result = findReels(obj[key])
+   if (result) {
+    return result
+   }
   }
+ }
 }
 
 export async function reelsOnClicked(target: HTMLAnchorElement) {
-  const final = (obj: DownloadParams) => {
-    if (target.className.includes('download-btn')) {
-      downloadResource(obj);
-    } else {
-      openInNewTab(obj.url);
-    }
-  };
-
-  const handleMedia = (media: Reels.Media) => {
-    const url =
-      media.video_versions?.[0].url || media.image_versions2.candidates[0].url;
-    final({
-      url: url,
-      username: media.user.username,
-      datetime: dayjs.unix(media.taken_at),
-      fileId: getMediaName(url),
-    });
-  };
-
-  const { reels_edges_data } = await chrome.storage.local.get([
-    'reels_edges_data',
-  ]);
-  const code = window.location.pathname.split('/').at(-2);
-  const media = new Map(reels_edges_data).get(code) as Reels.Media | undefined;
-  if (media) {
-    handleMedia(media);
-    return;
+ const final = (obj: DownloadParams) => {
+  if (target.className.includes('download-btn')) {
+   downloadResource(obj)
+  } else {
+   openInNewTab(obj.url)
   }
+ }
 
-  const scripts = await fetchHtml();
-  for (const script of [...window.document.scripts, ...scripts]) {
-    try {
-      const innerHTML = script.innerHTML;
-      const data = JSON.parse(innerHTML);
-      if (innerHTML.includes('xdt_api__v1__clips__home__connection_v2')) {
-        const res = findReels(data);
-        if (res) {
-          for (const item of res.edges) {
-            if (item.node.media.code === code) {
-              handleMedia(item.node.media);
-              return;
-            }
-          }
-        }
-      }
-    } catch {}
-  }
+ const handleMedia = (media: Reels.Media) => {
+  const url = media.video_versions?.[0].url || media.image_versions2.candidates[0].url
+  final({
+   url: url,
+   username: media.user.username,
+   datetime: dayjs.unix(media.taken_at),
+   fileId: getMediaName(url),
+  })
+ }
 
-  const wrapperNode = target.parentNode!.parentNode as HTMLDivElement;
+ const { reels_edges_data } = await chrome.storage.local.get(['reels_edges_data'])
+ const code = window.location.pathname.split('/').at(-2)
+ const media = new Map(reels_edges_data).get(code) as Reels.Media | undefined
+ if (media) {
+  handleMedia(media)
+  return
+ }
+
+ const scripts = await fetchHtml()
+ for (const script of [...window.document.scripts, ...scripts]) {
   try {
-    const res = await getUrlFromInfoApi(wrapperNode);
-    if (!res) return;
-    // console.log('url', res.url);s
-    final({
-      url: res.url,
-      username: res.owner,
-      datetime: dayjs.unix(res.taken_at),
-      fileId: getMediaName(res.url),
-    });
-  } catch (e: any) {
-    alert('Reels Download Failed!');
-    console.log(`Uncaught in postDetailOnClicked(): ${e}\n${e.stack}`);
-    return;
+   const innerHTML = script.innerHTML
+   const data = JSON.parse(innerHTML)
+   if (innerHTML.includes('xdt_api__v1__clips__home__connection_v2')) {
+    const res = findReels(data)
+    if (res) {
+     for (const item of res.edges) {
+      if (item.node.media.code === code) {
+       handleMedia(item.node.media)
+       return
+      }
+     }
+    }
+   }
+  } catch {}
+ }
+
+ const wrapperNode = target.parentNode!.parentNode as HTMLDivElement
+ try {
+  const res = await getUrlFromInfoApi(wrapperNode)
+  if (Array.isArray(res)) {
+   res.forEach((item) => {
+    handleMedia(item.res)
+   })
+   return
   }
+  if (!res) return
+  // console.log('url', res.url);s
+  final({
+   url: res.url,
+   username: res.owner,
+   datetime: dayjs.unix(res.taken_at),
+   fileId: getMediaName(res.url),
+  })
+ } catch (e: any) {
+  alert('Reels Download Failed!')
+  console.log(`Uncaught in postDetailOnClicked(): ${e}\n${e.stack}`)
+  return
+ }
 }
